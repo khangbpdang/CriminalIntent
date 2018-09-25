63bbd96bfbf628366f7d37ea942d4d1683598b61
khangdang <khangdang@mail.adephi.edu>
2018-09-25 01:01:07 -0400

    Initial commit

---
+++ app/src/main/java/khangdang/com/criminalintent/CrimeActivity.java
@@ -0,0 +1,17 @@
+package khangdang.com.criminalintent;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+
+import java.util.UUID;
+
+public class CrimeActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        UUID crimeId = (UUID)getIntent()
+                .getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
+    }
+}
